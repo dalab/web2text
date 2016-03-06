fed62d995e187044660d51ecb2569b96990988c7
Thijs Vogels <t.vogels@me.com>
2016-03-06 23:09:30 +0100

    Deny direct insert etc

--- labeling/lib/collections.js
+++ labeling/lib/collections.js
@@ -1,3 +1,26 @@
+Meteor.users.deny({
+  update() {
+    return true;
+  }
+});
+
 Documents = new Mongo.Collection("documents");
+Documents.deny({
+  insert() { return true; },
+  update() { return true; },
+  delete() { return true; },
+});
+
 Datasets = new Mongo.Collection("datasets");
+Datasets.deny({
+  insert() { return true; },
+  update() { return true; },
+  delete() { return true; },
+});
+
 Labels = new Mongo.Collection("labels");
+Labels.deny({
+  insert() { return true; },
+  update() { return true; },
+  delete() { return true; },
+});
