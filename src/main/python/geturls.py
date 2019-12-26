9ed0067f7c941c38cfe9a2897a88921e7943234f
Cesc Canet <canet.cesc@gmail.com>
2019-12-26 01:57:23 +0100

    Add geturls

---
+++ src/main/python/geturls.py
@@ -0,0 +1,16 @@
+import csv
+import sys
+import hashlib
+import urllib
+
+inputFileName = "./articles3.csv"
+csv.field_size_limit(sys.maxsize)
+with open(inputFileName, 'r') as f:
+  reader = csv.reader(f)
+  a_line_after_header = next(reader)
+  for row in reader:
+    url = row[8]
+    if len(url) > 7:
+      outputFileName =  hashlib.sha1(url).hexdigest()
+      print("URL:" + url + ", Hash: " + outputFileName)
+      urllib.urlretrieve(url, filename="html/" + outputFileName + ".html")
