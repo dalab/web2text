f20d1a30cb4f712e10ad97eebe01fbf4a6557bec
Cesc Canet <canet.cesc@gmail.com>
2020-01-04 03:39:39 +0100

    Ramp up TF 2 model

--- src/main/python/nn.py
+++ src/main/python/nn.py
@@ -4,26 +4,30 @@ import numpy as np
 import pandas as pd
 import os
 import time
-#from math import floor, ceil
-#from pylab import rcParams
+from math import floor, ceil
+import sklearn as sk
+from sklearn.metrics import confusion_matrix
 
-NUM_LABELS = 1  # The number of labels.
-BATCH_SIZE = 1 # The number of training examples to use per training step.
+NUM_LABELS = 1
+BATCH_SIZE = 1000 # The number of training examples to use per training step.
 TRAINING_EPOCHS = 5000
-DISPLAY_STEP = 1000
+DISPLAY_STEP = 100
 LEARNING_RATE=0.0001
 KEEP_PROB=0.8
-NUM_HIDDEN = 10
 NUM_FEATURES = 137
+NUM_HIDDEN = NUM_FEATURES
 TRAIN_AND_TEST_FILE_PATH = '../../../public/train_and_test/'
-TRAIN_PAGES_SIZE = 800 # There are 76,968 files
-TEST_PAGES_SIZE = 200
+TRAIN_SIZE = 0.8
+NUM_FILES_TO_READ = 20 # There are 76,968 files. 100 files take about 20 minutes with 5000 epochs
 RANDOM_STATE = 1
 
-def read_csvs(path, skip_pages=0,pages_size=0):
+def encode(series):
+    return pd.get_dummies(series.astype(str))
+
+def read_csvs(path):
     file_list = [os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
     first = True
-    for i in range(skip_pages,skip_pages+pages_size):
+    for i in range(NUM_FILES_TO_READ):
         #print(f"i={i}")
         file = file_list[i]
         #print(f"reading file:{file}")
@@ -92,19 +96,21 @@ if __name__ == '__main__':
     np.random.seed(RANDOM_STATE)
     tf.set_random_seed(RANDOM_STATE)
     start_time = time.clock()
-    train_df = read_csvs(TRAIN_AND_TEST_FILE_PATH,0,TRAIN_PAGES_SIZE)
-    test_df = read_csvs(TRAIN_AND_TEST_FILE_PATH,TRAIN_PAGES_SIZE,TEST_PAGES_SIZE)
+    train_and_test_df = read_csvs(TRAIN_AND_TEST_FILE_PATH)
+    train_cnt = floor(train_and_test_df.shape[0] * TRAIN_SIZE)
+    train_df = train_and_test_df.iloc[0:train_cnt]
+    test_df = train_and_test_df.iloc[train_cnt:]
     train_x = train_df.drop(columns=['contains_author'])
-    train_y = train_df['contains_author']
+    train_y = encode(train_df['contains_author'])
     test_x = test_df.drop(columns=['contains_author'])
-    test_y = test_df['contains_author']
+    test_y = encode(test_df['contains_author'])
     print(f"train_x.shape: {train_x.shape}, train_y.shape={train_y.shape}")
     print(f"test_x.shape: {test_x.shape}, test_y.shape={test_y.shape}")
-    print(f"Read training and test files: {time.clock() - start_time} seconds")
+    print(f"Read {NUM_FILES_TO_READ} training and test files: {time.clock() - start_time} seconds")
     start_time = time.clock()
     n_hidden_1 = NUM_HIDDEN
     n_input = train_x.shape[1]
-    n_classes = 1 # train_y.shape[1]
+    n_classes = 2 # train_y.shape[1]
     print(f"n_input={n_input}, n_classes={n_classes}, n_hidden_1={n_hidden_1}")
     weights = {
         'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
@@ -114,12 +120,13 @@ if __name__ == '__main__':
         'b1': tf.Variable(tf.random_normal([n_hidden_1])),
         'out': tf.Variable(tf.random_normal([n_classes]))
     }
-    keep_prob = tf.placeholder("float")
-    x = tf.placeholder("float", [None, n_input], name="cesc_x")
-    y = tf.placeholder("float", [n_classes], name="cesc_y")
+    keep_prob = tf.placeholder("float", name="keep_prob")
+    x = tf.placeholder("float", [None, n_input], name="x")
+    y = tf.placeholder("float", [None, n_classes], name="y")
     predictions = multilayer_perceptron(x, weights, biases, keep_prob)
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=predictions, labels=y))
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=predictions, labels=y))
     optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(cost)
+    saver = tf.train.Saver()
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         for epoch in range(TRAINING_EPOCHS):
@@ -129,8 +136,6 @@ if __name__ == '__main__':
             y_batches = np.array_split(train_y, total_batch)
             for i in range(total_batch):
                 batch_x, batch_y = x_batches[i], y_batches[i]
-                #print(f"batch_x.shape={batch_x.shape}")
-                #print(f"batch_y.shape={batch_y.shape}")
                 _, c = sess.run([optimizer, cost],
                                 feed_dict={
                                     x: batch_x,
@@ -140,8 +145,25 @@ if __name__ == '__main__':
                 avg_cost += c / total_batch
             if epoch % DISPLAY_STEP == 0:
                 print("Epoch:", '%04d' % (epoch+1), "cost=", \
-                      "{:.9f}".format(avg_cost))
+                      "{:.9f}".format(avg_cost), " time=", "{:.2f}".format(time.clock() - start_time), " sec.")
+
+        print(f"Training time: {time.clock() - start_time} seconds")
         print("Optimization Finished!")
+
+        save_path = saver.save(sess, "trained_model_all_the_news_split/model.ckpt")
+
         correct_prediction = tf.equal(tf.argmax(predictions, 1), tf.argmax(y, 1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
         print("Accuracy:", accuracy.eval({x: test_x, y: test_y, keep_prob: 1.0}))
+
+        y_p = tf.argmax(predictions, 1)
+        val_accuracy, y_pred = sess.run([accuracy, y_p], feed_dict={x:test_x, y:test_y, keep_prob: 1.0})
+
+        print(f"Test set Accuracy: {val_accuracy}")
+        y_true = np.argmax(test_y,1)
+        print(f"Precision: {sk.metrics.precision_score(y_true, y_pred)}")
+        print(f"Recall: {sk.metrics.recall_score(y_true, y_pred)}")
+        print(f"F1 Score: {sk.metrics.f1_score(y_true, y_pred)}")
+        print("Confusion Matrix")
+        print(sk.metrics.confusion_matrix(y_true, y_pred))
+        fpr, tpr, tresholds = sk.metrics.roc_curve(y_true, y_pred)
